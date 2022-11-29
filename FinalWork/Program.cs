a468aac3c828c709d8a8a201c39730cabaef4d83
BoT143 <you@example.com>
2022-11-29 11:09:21 +0300

    All final work DONE ! )

---
+++ FinalWork/Program.cs
@@ -0,0 +1,28 @@
+﻿
+void CheckingTheLength(string[] array, string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            array1[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("Words and numbers less than or equal to 3: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = new string[5] {"Peach", "-3", "School", "you", "I"};
+string[] array1 = new string[array.Length];
+CheckingTheLength(array, array1);
+PrintArray(array1);
